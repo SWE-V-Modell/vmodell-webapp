b497283107713a5ba19ee043c878dab036099d51
Jonasdero <jonas.roser@gmail.com>
2019-01-28 08:35:25 +0100

    Navigation läuft wieder (Kein lazyloading mehr)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,11 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { ComponentsModule } from './components/components.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { LoginModule } from './login/login.module';
+import { StatisticsModule } from './statistics/statistics.module';
+import { VeranstaltungModule } from './veranstaltung/veranstaltung.module';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
@@ -24,7 +29,12 @@ import { ComponentsModule } from './components/components.module';
 
     // Modules
     ComponentsModule,
-    CoreModule
+    CoreModule,
+    DashboardModule,
+    LoginModule,
+    StatisticsModule,
+    VeranstaltungModule,
+    AdminModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
